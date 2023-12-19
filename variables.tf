119433de62c4a2245ed88fe7ae301bbc340631c5
Yuma Ishikawa <yuma.fuu05@gmail.com>
2023-12-19 14:09:19 +0900

    [add] module

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type = string
+  description = "The name of the repository"
+}
+
+variable "expire_count" {
+  type = number
+  description = "The number of images to keep in the repository"
+}
